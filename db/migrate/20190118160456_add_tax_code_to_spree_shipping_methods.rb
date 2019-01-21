f33bb3473d830d99a45044961f49afe6cc62148f
piotr.pasciak <piotr.pasciak@sparksolutions.co>
2019-01-21 10:58:57 +0100

    Use SpreeExtension::Migration base class in migrations

--- db/migrate/20190118160456_add_tax_code_to_spree_shipping_methods.rb
+++ db/migrate/20190118160456_add_tax_code_to_spree_shipping_methods.rb
@@ -1,4 +1,4 @@
-class AddTaxCodeToSpreeShippingMethods < ActiveRecord::Migration[5.2]
+class AddTaxCodeToSpreeShippingMethods < SpreeExtension::Migration[4.2]
   def change
     return if column_exists?(:spree_shipping_methods, :tax_code)
 
