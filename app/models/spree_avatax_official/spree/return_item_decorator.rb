2a99635b442bbfbee4200c9cd8d5f427f44a3718
symarys <maria.syczewska@sparksolutions.co>
2019-02-19 10:12:05 +0100

    Prepend return item

---
+++ app/models/spree_avatax_official/spree/return_item_decorator.rb
@@ -0,0 +1,19 @@
+module SpreeAvataxOfficial
+  module Spree
+    module ReturnItemDecorator
+      def self.prepended(base)
+        base.state_machine.after_transition to: :received, do: :refund_in_avatax
+      end
+
+      private
+
+      def refund_in_avatax
+        return unless SpreeAvataxOfficial::Config.enabled
+
+        SpreeAvataxOfficial::Transactions::RefundService.call(refundable: self)
+      end
+    end
+  end
+end
+
+::Spree::ReturnItem.prepend(::SpreeAvataxOfficial::Spree::ReturnItemDecorator) if 'Spree::ReturnItem'.safe_constantize
