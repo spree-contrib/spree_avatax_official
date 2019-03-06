5fa675f90becc1dc5b85a5d1f839bf4cfb9208f0
piotrekpasciak <piotrek.pasciak@gmail.com>
2019-03-06 15:46:48 +0100

    Grab Spree::TaxRate amount from AvaTax

--- app/models/spree_avatax_official/configuration.rb
+++ app/models/spree_avatax_official/configuration.rb
@@ -1,11 +1,12 @@
 module SpreeAvataxOfficial
   class Configuration < Spree::Preferences::Configuration
-    preference :company_code,      :string,  default: ENV['AVATAX_COMPANY_CODE']
-    preference :enabled,           :boolean, default: false
-    preference :ship_from_address, :hash,    default: {}
-    preference :log,               :boolean, default: true
-    preference :log_to_stdout,     :boolean, default: false
-    preference :log_file_name,     :string,  default: 'avatax.log'
-    preference :log_frequency,     :string,  default: 'weekly'
+    preference :company_code,       :string,  default: ENV['AVATAX_COMPANY_CODE']
+    preference :enabled,            :boolean, default: false
+    preference :ship_from_address,  :hash,    default: {}
+    preference :log,                :boolean, default: true
+    preference :log_to_stdout,      :boolean, default: false
+    preference :log_file_name,      :string,  default: 'avatax.log'
+    preference :log_frequency,      :string,  default: 'weekly'
+    preference :show_rate_in_label, :boolean, default: false
   end
 end
