4ec96282eb84a5adf67b708e8d021fb96906639c
piotrekpasciak <piotrek.pasciak@gmail.com>
2019-03-13 12:15:30 +0100

    Make factories comply with null: false constrains on spree_calculators table

--- spec/factories/spree/avatax_shipping_method_factory.rb
+++ spec/factories/spree/avatax_shipping_method_factory.rb
@@ -3,7 +3,6 @@ FACTORY_BOT_CLASS.define do
     zones      { [Spree::Zone.find_by(name: 'GlobalZone') || create(:avatax_global_zone, default_tax: true)] }
     name       { 'AvaTax Ground' }
     display_on { 'both' }
-    association(:calculator, factory: :shipping_calculator, strategy: :create)
 
     transient do
       tax_included      { false }
@@ -13,10 +12,23 @@ FACTORY_BOT_CLASS.define do
     before(:create) do |shipping_method, evaluator|
       create(:country) if Spree::Country.count.zero?
 
-      shipping_tax_rate            = create(:avatax_tax_rate, :shipping_tax_rate, included_in_price: evaluator.tax_included)
-      shipping_method.tax_category = shipping_tax_rate.tax_category if evaluator.with_tax_category
+      shipping_tax_rate              = create(:avatax_tax_rate, :shipping_tax_rate, included_in_price: evaluator.tax_included)
+      shipping_method.tax_category   = shipping_tax_rate.tax_category if evaluator.with_tax_category
+      shipping_method.calculator     = if Gem::Version.new(Spree.version) >= Gem::Version.new('2.3.0')
+                                         build(:shipping_calculator, calculable: shipping_method, preferences: { currency: 'USD', amount: 10.0 })
+                                       else
+                                         build(:shipping_calculator, calculable: shipping_method)
+                                       end
+
       if shipping_method.shipping_categories.empty?
         shipping_method.shipping_categories << (Spree::ShippingCategory.first || create(:shipping_category))
+      end
+    end
+
+    if Gem::Version.new(Spree.version) < Gem::Version.new('2.3.0')
+      after(:create) do |shipping_method|
+        shipping_method.calculator.preferred_currency = 'USD'
+        shipping_method.calculator.preferred_amount   = 10.0
       end
     end
   end
--- spec/factories/spree/avatax_tax_rate_factory.rb
+++ spec/factories/spree/avatax_tax_rate_factory.rb
@@ -9,13 +9,15 @@ FACTORY_BOT_CLASS.define do
         create(:avatax_tax_category, :clothing)
     end
 
-    association(:calculator, factory: :avatax_transaction_calculator)
-
     trait :shipping_tax_rate do
       tax_category do
         Spree::TaxCategory.find_by(tax_code: ::Spree::TaxCategory::DEFAULT_TAX_CODES['Spree::Shipment']) ||
           create(:avatax_tax_category, :shipping)
       end
+    end
+
+    before(:create) do |tax_rate|
+      tax_rate.calculator = build(:avatax_transaction_calculator, calculable: tax_rate)
     end
   end
 end
