b8b47f040824364582c752b163e465fae325e8fb
Yuriy Kushnir <kushnir.yb@gmail.com>
2019-03-25 16:08:16 +0100

    Change constants definition :umbrella:

--- app/models/spree/tax_category_decorator.rb
+++ app/models/spree/tax_category_decorator.rb
@@ -1,10 +1,12 @@
 module SpreeAvataxOfficial
   module Spree
     module TaxCategoryDecorator
-      DEFAULT_TAX_CODES = {
-        'LineItem' => 'P0000000',
-        'Shipment' => 'FR'
-      }.freeze
+      def self.prepended(base)
+        base.const_set 'DEFAULT_TAX_CODES', {
+          'LineItem' => 'P0000000',
+          'Shipment' => 'FR'
+        }.freeze
+      end
     end
   end
 end
