0d81978383452331a65e05d9ba2741f81835a01f
symarys <maria.syczewska@sparksolutions.co>
2019-05-29 14:08:34 +0200

    Remove redundant migrations

--- db/migrate/20190118160456_add_tax_code_to_spree_shipping_methods.rb
+++
@@ -1,7 +0,0 @@
-class AddTaxCodeToSpreeShippingMethods < SpreeExtension::Migration[4.2]
-  def change
-    return if column_exists?(:spree_shipping_methods, :tax_code)
-
-    add_column :spree_shipping_methods, :tax_code, :string
-  end
-end
