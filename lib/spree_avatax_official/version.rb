5d0a1648e1383170937d28207f23292f86f6a180
Damian Legawiec <damian@sparksolutions.co>
2019-07-26 12:42:53 +0200

    Bump version to 1.0.0 :tada:

--- lib/spree_avatax_official/version.rb
+++ lib/spree_avatax_official/version.rb
@@ -8,8 +8,8 @@ module SpreeAvataxOfficial
   end
 
   module VERSION
-    MAJOR = 0
-    MINOR = 9
+    MAJOR = 1
+    MINOR = 0
     TINY  = 0
 
     STRING = [MAJOR, MINOR, TINY].compact.join('.')
