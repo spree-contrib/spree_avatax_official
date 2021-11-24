43871391d89bbdd315f1f095d5a4b55c1a3e14e8
agnieszkajacek <agnieszka.jacek90@gmail.com>
2021-11-24 15:57:14 +0100

    Remove SpreeAvataxOfficial::Configuration from engine

--- lib/spree_avatax_official/engine.rb
+++ lib/spree_avatax_official/engine.rb
@@ -8,10 +8,6 @@ module SpreeAvataxOfficial
 
     config.autoload_paths += %W[#{config.root}/lib]
 
-    initializer 'spree_avatax_official.environment', before: :load_config_initializers do |_app|
-      SpreeAvataxOfficial::Config = SpreeAvataxOfficial::Configuration.new
-    end
-
     # use rspec for tests
     config.generators do |g|
       g.test_framework :rspec
