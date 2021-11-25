5aaadd5efc7b88e14755d4c00a078cef36c4d165
agnieszkajacek <agnieszka.jacek90@gmail.com>
2021-11-25 11:58:38 +0100

    Move configuration to another file

---
+++ config/spree_avatax_config.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require_relative "../app/models/spree_avatax_official/configuration"
+require_relative "../app/models/spree_avatax_official/calculator/avatax_transaction_calculator"
+
+Rails.application.config.spree = Spree::Core::Environment.new
+SpreeAvataxOfficial::Config = SpreeAvataxOfficial::Configuration.new
