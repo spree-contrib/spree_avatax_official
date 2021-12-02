0493a7d0ed192b9142e73b83e4b514f8040a6a0b
agnieszkajacek <agnieszka.jacek90@gmail.com>
2021-12-02 17:37:35 +0100

    Fix pipeline

--- lib/generators/spree_avatax_official/install/templates/config/initializers/spree_avatax_official.rb
+++ lib/generators/spree_avatax_official/install/templates/config/initializers/spree_avatax_official.rb
@@ -1,3 +1,7 @@
+module SpreeAvataxOfficial
+  Config = SpreeAvataxOfficial::Configuration.new
+end
+
 AvaTax.configure do |config|
   config.endpoint = 'https://sandbox-rest.avatax.com' # This endpoint is used for testing and should be replaced
   config.username = ''
