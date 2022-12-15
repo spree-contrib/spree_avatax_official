9ba6f9316cea28375ab2d6249fef2d0cd7c0f587
Rafal Cymerys <rafal@upsidelab.io>
2022-12-15 16:34:59 +0100

    Update initializers to work with Rails 7

--- lib/generators/spree_avatax_official/install/templates/config/initializers/spree_avatax_official.rb
+++ lib/generators/spree_avatax_official/install/templates/config/initializers/spree_avatax_official.rb
@@ -1,9 +1,11 @@
-# Optional configuration fields
-SpreeAvataxOfficial::Config.log                        = true
-SpreeAvataxOfficial::Config.log_to_stdout              = false
-SpreeAvataxOfficial::Config.log_file_name              = 'avatax.log'
-SpreeAvataxOfficial::Config.log_frequency              = 'weekly'
-SpreeAvataxOfficial::Config.max_retries                = 2
-SpreeAvataxOfficial::Config.open_timeout               = 2.0
-SpreeAvataxOfficial::Config.read_timeout               = 6.0
-SpreeAvataxOfficial::Config.show_rate_in_label         = false
+Rails.application.config.after_initialize do
+  # Optional configuration fields
+  SpreeAvataxOfficial::Config.log                        = true
+  SpreeAvataxOfficial::Config.log_to_stdout              = false
+  SpreeAvataxOfficial::Config.log_file_name              = 'avatax.log'
+  SpreeAvataxOfficial::Config.log_frequency              = 'weekly'
+  SpreeAvataxOfficial::Config.max_retries                = 2
+  SpreeAvataxOfficial::Config.open_timeout               = 2.0
+  SpreeAvataxOfficial::Config.read_timeout               = 6.0
+  SpreeAvataxOfficial::Config.show_rate_in_label         = false
+end
---
+++ lib/spree_avatax_official/configuration.rb
@@ -0,0 +1,20 @@
+module SpreeAvataxOfficial
+  class Configuration < ::Spree::Preferences::Configuration
+    preference :address_validation_enabled, :boolean, default: false
+    preference :company_code,               :string,  default: ''
+    preference :enabled,                    :boolean, default: false
+    preference :ship_from_address,          :hash,    default: {}
+    preference :log,                        :boolean, default: true
+    preference :log_to_stdout,              :boolean, default: false
+    preference :log_file_name,              :string,  default: 'avatax.log'
+    preference :log_frequency,              :string,  default: 'weekly'
+    preference :max_retries,                :integer, default: 2
+    preference :open_timeout,               :decimal, default: 2.0
+    preference :read_timeout,               :decimal, default: 6.0
+    preference :show_rate_in_label,         :boolean, default: false
+    preference :account_number,             :string,  default: ''
+    preference :endpoint,                   :string,  default: Rails.env.production? ? 'https://rest.avatax.com' : 'https://sandbox-rest.avatax.com'
+    preference :license_key,                :string,  default: ''
+    preference :commit_transaction_enabled, :boolean, default: true
+  end
+end
