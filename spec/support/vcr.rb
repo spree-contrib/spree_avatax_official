196e1d8949575b43747118759bb7483cbb976a45
Damian Legawiec <damian@getvendo.com>
2024-04-25 18:40:32 +0200

    Update vcr.rb

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,3 +1,8 @@
+require 'vcr'
+require 'webmock/rspec'
+require 'uri'
+require 'webdrivers'
+
 VCR.configure do |config|
   config.allow_http_connections_when_no_cassette = false
   config.cassette_library_dir                    = 'spec/vcr'
@@ -6,6 +11,14 @@ VCR.configure do |config|
 
   config.configure_rspec_metadata!
   config.hook_into :webmock
+
+  # webdrivers fix
+  driver_hosts = Webdrivers::Common.subclasses.map { |driver| URI(driver.base_url).host }
+  driver_hosts << 'googlechromelabs.github.io'
+  driver_hosts << 'edgedl.me.gvt1.com'
+  driver_hosts << 'storage.googleapis.com'
+
+  config.ignore_hosts(*driver_hosts)
 
   config.filter_sensitive_data('<AVATAX_TOKEN>') do |interaction|
     interaction.request.headers['Authorization'].first
